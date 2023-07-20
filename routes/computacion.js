e1e90dc3e1d8e5319cb81981183d712e8c3f3d5a
BraianFG <55467665+BraianFG@users.noreply.github.com>
2023-07-20 16:12:16 -0300

    Add files via upload

---
+++ routes/computacion.js
@@ -0,0 +1,185 @@
+const router = require("express").Router();
+const { connectToMongoDB, disconnectFromMongoDB } = require("../src/mongodb");
+
+const databaseName = 'computacion';
+
+router.get('/', async (req, res) => {
+    try {
+        const client = await connectToMongoDB();
+        if (!client) {
+            res.status(500).send('Error al conectar a la base de datos');
+            return;
+        }
+
+        const db = client.db(databaseName);
+        const productos = await db.collection('computacion').find().toArray();
+        await disconnectFromMongoDB();
+
+        res.send(productos);
+    } catch (error) {
+        console.error('Error en la ruta /:', error);
+        res.status(500).send('Error al obtener los productos');
+    }
+});
+
+
+router.get('/id/:id', async (req, res) => {
+    try {
+        const productId = req.params.id;
+        const client = await connectToMongoDB();
+        if (!client) {
+            res.status(500).send('Error al conectar a la base de datos');
+            return;
+        }
+
+        const db = client.db(databaseName);
+        const producto = await db.collection('computacion').findOne({ _id: ObjectId(productId) });
+        await disconnectFromMongoDB();
+
+        if (!producto) {
+            res.status(404).send('Producto no encontrado');
+            return;
+        }
+
+        res.send(producto);
+    } catch (error) {
+        console.error('Error en la ruta /id/:id:', error);
+        res.status(500).send('Error al obtener el producto');
+    }
+});
+
+router.get('/nombre/:nombre', async (req, res) => {
+    try {
+        const productName = req.params.nombre;
+        const client = await connectToMongoDB();
+        if (!client) {
+            res.status(500).send('Error al conectar a la base de datos');
+            return;
+        }
+
+        const db = client.db(databaseName);
+        const productos = await db
+            .collection('computacion')
+            .find({ nombre: { $regex: productName, $options: 'i' } })
+            .toArray();
+
+        await disconnectFromMongoDB();
+        res.send(productos);
+    } catch (error) {
+        console.error('Error en la ruta /nombre/:nombre:', error);
+        res.status(500).send('Error al obtener los productos por nombre');
+    }
+});
+
+
+router.get('/precio/:precio', async (req, res) => {
+    try {
+        const productoPrecio = parseInt(req.params.precio) || 0;
+        console.log(req.params.precio);
+        const client = await connectToMongoDB();
+        if (!client) {
+            res.status(500).send("Error al conectar a la base de datos");
+            return;
+        }
+
+        const db = client.db(databaseName);
+        const producto = await db.collection('computacion').find({ importe: { $gte: parseInt(productoPrecio) } }).toArray();
+
+        await disconnectFromMongoDB();
+
+        if (producto.length === 0) {
+            res.status(404).send(`No es posible encontrar productos con un precio mayor o igual a $${productoPrecio}`);
+        } else {
+            res.send(producto);
+        }
+    } catch (error) {
+        console.error('Error en la ruta /precio/:precio:', error);
+        res.status(500).send('Error al obtener los productos por precio');
+    }
+});
+
+router.post('/', async (req, res) => {
+    const nuevoProducto = req.body;
+    console.log(nuevoProducto);
+    if (nuevoProducto === undefined) {
+        res.status(400).send('Error de formato al crearlo');
+    }
+    const client = await connectToMongoDB();
+    if (!client) {
+        res.status(500).send('Error al conectarse al servidor');
+    }
+    const collection = client.db('computacion').collection('computacion');
+    collection.insertOne(nuevoProducto)
+        .then(() => {
+            console.log('Se ha creado una nuevo producto');
+            res.status(200).send(nuevoProducto);
+        })
+        .catch(error => {
+            console.error(error);
+        })
+        .finally(() => {
+            client.close();
+        });
+});
+
+router.put('/id/:id', async (req, res) => {
+    const id = req.params.id;
+    const nuevosDatos = req.body;
+
+    if (!nuevosDatos) {
+        res.status(400).send('Error en el formato de datos recibidos.');
+    }
+
+    const client = await connectToMongoDB();
+    if (!client) {
+        res.status(500).send('Error al conectarse a MongoDB');
+    }
+
+    const collection = client.db(databaseName).collection('computacion');
+    collection.updateOne({ id: parseInt(id) }, { $set: nuevosDatos })
+        .then(() => {
+            console.log('Producto modificado:');
+            res.status(200).send(nuevosDatos);
+        })
+        .catch((error) => {
+            res.status(500).json({ descripcion: 'Error al modificar el producto' });
+        })
+        .finally(() => {
+            client.close();
+        });
+});
+
+router.delete('/id/:id', async (req, res) => {
+    const id = req.params.id;
+    if (!id) {
+        return res.status(400).send('El formato de datos es incorrecto o inválido.');
+    }
+
+    const client = await connectToMongoDB();
+    if (!client) {
+        return res.status(500).send('Error al conectarse a MongoDB');
+    }
+
+    client.connect()
+        .then(() => {
+            const collection = client.db(databaseName).collection('computacion');
+            return collection.deleteOne({ id: parseInt(id) });
+        })
+        .then((resultado) => {
+            if (resultado.deletedCount === 0) {
+                res.status(404).send(`No se encontró producto con ID proporcionado: ${id}`);
+            } else {
+                console.log('Producto eliminado.');
+                res.status(204).send();
+            }
+        })
+        .catch((error) => {
+            console.error(error);
+            res.status(500).send('Se produjo un error al intentar eliminar el producto.');
+        })
+        .finally(() => {
+            client.close();
+        });
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,80 +1,27 @@
-const express = require('express')
+const express = require('express');
+const dotenv = require('dotenv');
 const { connectToMongoDB, disconnectFromMongoDB } = require('./src/mongodb');
-const dotenv = require('dotenv')
-const path = require('path')
-const app = express()
+const path = require('path');
+const computacionRouter = require('./routes/computacion');
 dotenv.config();
-const port = 3008
+const app = express();
+const PORT = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, 'public/admin')));
+app.use(express.json());
 
 app.use((req, res, next) => {
-  res.header("Content-Type", "application/json; charset=utf-8");
-  next();
+    res.header("Content-Type", "application/json; charset=utf-8");
+    next();
 });
 
 app.get('/', (req, res) => {
-  res.status(200).end(`welcome to the computacion home`)
+    res.status(200).send(`Welcome to the Computacion home!`);
 });
 
-app.get('/computacion', async (req, res) => {
-  const client = await connectToMongoDB();
-  if (!client) {
-    res.status(500).send('error al conectar a la base mongo DB');
-    return;
-  }
-  const db = client.db('computacion');
-  const computacion = await db.collection('computacion').find().toArray();
-  await disconnectFromMongoDB();
-  res.send(computacion);
-});
+app.use('/computacion', computacionRouter);
 
-app.get('/computacion/:id', async (req, res) => {
-  const computacionId = parseInt(req.params.id) || 0;
-  const client = await connectToMongoDB();
-  if (!client) {
-    res.status(500).send("error al conectar a la base mongo DB");
-    return;
-  }
-  const db = client.db('computacion');
-  const computacion = await db.collection("computacion").findOne({ id: computacionId });
-  await disconnectFromMongoDB();
-  !computacion ? res.status(404).send(`no es posible encontrar la  con id: ${computacionId}`)
-    : res.send(computacion);
-    document.querySelector("#codigo").innerHTML=`${computacionId}`;
+app.listen(PORT, async () => {
+    await connectToMongoDB();
+    console.log(`Servidor escuchando en el puerto ${PORT}`);
 });
-
-app.get('/computacion/:nombre', async (req, res) => {
-  const computacionNombre = req.params.nombre;
-  console.log(computacionNombre);
-  const client = await connectToMongoDB();
-  if (!client) {
-    res.status(500).send("error al conectar a la base mongo DB");
-    return;
-  }
-  const db = client.db('computacion');
-  const computacion = await db.collection("computacion").findOne({ nombre: computacionNombre });
-  await disconnectFromMongoDB();
-  (!computacion) ? res.status(404).send(`no es posible encontrar la computacion con un nombre: ${computacionNombre}`)
-    : res.send(computacion);
-});
-
-app.get('/computacion/:precio', async (req, res) => {
-  const computacionPrecio = parseInt(req.params.precio) || 0;
-  console.log(req.params.precio);
-  const client = await connectToMongoDB();
-  if (!client) {
-    res.status(500).send("error al conectar a la base mongo DB");
-    return;
-  }
-  const db = client.db("computacion");
-  const computacion = await db.collection("computacion").find({ importe: { $gte: computacionPrecio } }).toArray();
-  await disconnectFromMongoDB();
-  computacion.length === 0 ? res.status(404).send(`no es posible encontrar computacion con un precio mayor o igual a $${computacionPrecio}`)
-    : res.send(computacion);
-});
-
-app.listen(port || 3008, () => {
-  console.log(`escuchando el puerto ${port}`);
-});
-
